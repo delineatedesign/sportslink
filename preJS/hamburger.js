75f9b32dfc361fa742faf02b229ef80155af523d
ohsoren <hello@sorenfife.co.uk>
2016-07-18 10:02:00 +0100

    monday morning kettle time

--- preJS/hamburger.js
+++ preJS/hamburger.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $('.nav-toggle').on('click', function() {
     event.preventDefault();
     $(this).toggleClass('active');
-    //$('.right ul').toggleClass("sl-Nav-hidden");
+    $('body').attr('data-mobile-nav', $('body').attr('data-mobile-nav') == 'hidden' ? 'visible' : 'hidden')
   });
 });
--- preJS/index.js
+++ preJS/index.js
@@ -18,13 +18,16 @@ $(window).scroll(function () {
 $(document).ready(function() {
 
     $(function() {
-      $('a[href*="#"]:not([href="#"])').click(function() {
+      $('a[href*="#"]:not([href="#"])').on('click', function() {
+        $('body').attr('data-mobile-nav','hidden');
+        $('.nav-toggle').removeClass('active');
+
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
           if (target.length) {
             $('html, body').animate({
-              scrollTop: target.offset().top
+              scrollTop: target.offset().top - 88
             }, 1000);
             return false;
           }
